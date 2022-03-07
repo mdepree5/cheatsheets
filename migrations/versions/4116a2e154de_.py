0648e60691de3b333707d1a132bba1f50335f552
James Tuttle <jaymz69@yahoo.com>
2022-03-07 14:30:35 -0800

    added a seeder file for a simple git.

--- migrations/versions/4116a2e154de_.py
+++ migrations/versions/4116a2e154de_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 15736e78192e
+Revision ID: 4116a2e154de
 Revises: 
-Create Date: 2022-03-04 16:54:34.750607
+Create Date: 2022-03-07 10:31:59.444031
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '15736e78192e'
+revision = '4116a2e154de'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -29,33 +29,33 @@ def upgrade():
     )
     op.create_table('cheatsheets',
     sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('owner_id', sa.Integer(), nullable=False),
     sa.Column('title', sa.String(length=50), nullable=False),
     sa.Column('description', sa.String(length=255), nullable=False),
     sa.Column('dependencies', sa.Text(), nullable=False),
-    sa.Column('media_url', sa.String(length=255), nullable=False),
+    sa.Column('media_url', sa.String(length=255), nullable=True),
     sa.Column('created_at', sa.DateTime(), nullable=False),
     sa.Column('updated_at', sa.DateTime(), nullable=True),
-    sa.Column('owner_id', sa.Integer(), nullable=False),
     sa.ForeignKeyConstraint(['owner_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_table('comments',
     sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('writer_id', sa.Integer(), nullable=False),
+    sa.Column('cheatsheet_id', sa.Integer(), nullable=False),
     sa.Column('content', sa.Text(), nullable=False),
     sa.Column('created_at', sa.DateTime(), nullable=False),
     sa.Column('updated_at', sa.DateTime(), nullable=True),
-    sa.Column('writer_id', sa.Integer(), nullable=False),
-    sa.Column('cheatsheet_id', sa.Integer(), nullable=False),
     sa.ForeignKeyConstraint(['cheatsheet_id'], ['cheatsheets.id'], ),
     sa.ForeignKeyConstraint(['writer_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_table('steps',
     sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('cheatsheet_id', sa.Integer(), nullable=False),
     sa.Column('title', sa.String(length=255), nullable=False),
     sa.Column('content', sa.Text(), nullable=False),
-    sa.Column('media_url', sa.String(length=255), nullable=False),
-    sa.Column('cheatsheet_id', sa.Integer(), nullable=False),
+    sa.Column('media_url', sa.String(length=255), nullable=True),
     sa.ForeignKeyConstraint(['cheatsheet_id'], ['cheatsheets.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
