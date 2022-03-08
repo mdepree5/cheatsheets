de2cce4f9b195889deb4e197710cd4bf8e809767
daniel <danielthai14@gmail.com>
2022-03-08 16:36:25 -0700

    minor comments css

--- react-app/src/components/Comments/comments.js
+++ react-app/src/components/Comments/comments.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { addComment, getComment, editComment, deleteComment } from '../../store/comments';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams, useHistory } from 'react-router-dom';
+import './comments.css'
 
 
 function CommentsComponent({comments, cheatsheetId}) {
@@ -21,26 +22,33 @@ function CommentsComponent({comments, cheatsheetId}) {
     console.log('maybe we will get the thing we want...',commentsObj)
 
 
+    const [comment, setComment] = useState('')
+
+
     // const sessionUser = useSelector((state) => state?.session?.user);
     // const commentVals = Object.values(commentsObj)
 
 
     return (
         <>
-        <div className='comments_container'>
-            <div className='view_comments'>
+            <div className='comments_container'>
+                <div className='view_comments'>
+
+                </div>
+                <form className='post_comment_form'>
+                    <textarea className='post_comment_area' cols="50" rows="5"></textarea>
+                    <button>new comment</button>
+                </form>
+
+
+
+                <ul>
+                    {comments?.map(comment => (
+                        <li className={'posted_comments'} key={comment?.id}>
+                    {comment?.content}</li>))}
+                </ul>
 
             </div>
-            <form className='post_comment_form'>
-                <textarea className='post_comment_area' cols="50" rows="5"></textarea>
-                <button>new comment</button>
-            </form>
-
-
-
-            <div>{comments?.map(comment => (<div key={comment?.id} >{comment?.content}</div>))}</div>
-
-        </div>
         </>
     )
 }
