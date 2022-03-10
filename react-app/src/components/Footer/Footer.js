34a7b454b746de59be1c6e3a794221bfe2c3cbc6
James <jaymz69@yahoo.com>
2022-03-10 09:55:53 -0800

    saving footer for pull to main.

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import './Footer.css';
 
 
 const Footer = () => {
     return (
-        <div>
-            <h1>Footer</h1>
+        <div className="footer-container">
+            <h1>About Us</h1>
+            <div className="footer">
+                <div id='J'>
+                    <a href='https://github.com/jjtuttle'><FontAwesomeIcon icon={faGithub} />James</a>
+                </div>
+                <div id='K'>Y</div>
+            </div>
         </div>
     );
 };
