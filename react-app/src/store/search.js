159df64109e82a4c30087aba3fa598deedd34895
daniel <danielthai14@gmail.com>
2022-03-10 15:43:35 -0700

    search reducer

---
+++ react-app/src/store/search.js
@@ -0,0 +1,33 @@
+const LOAD_SEARCH_RESULTS = 'search/LOAD_SEARCH_RESULTS';
+
+const loadSearchResults = (cheatsheets) => ({
+    type: LOAD_SEARCH_RESULTS,
+    cheatsheets
+})
+
+export const getSearch = (keyword) => async (dispatch) => {
+    const response = await fetch(`/api/search`, {
+        method: 'GET'
+    });
+
+    if (response.ok) {
+        const data = await response.json();
+        dispatch(getSearch(data));
+        return data;
+    }
+    return response
+}
+
+const searchReducer = (state = {}, action) => {
+    let newState;
+    switch (action.type) {
+        case LOAD_SEARCH_RESULTS:
+            newState = {};
+            action.cheatsheets['cheatsheet_by_title'].forEach((cheatsheet) => newState[cheatsheet.id] = cheatsheet);
+            return newState;
+        default:
+            return state
+    }
+}
+
+export default searchReducer
