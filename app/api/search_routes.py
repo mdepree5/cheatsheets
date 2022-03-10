2afcb81f1f4e0145cee693eafbea5660aaa1b298
daniel <danielthai14@gmail.com>
2022-03-10 16:47:16 -0700

    backend change

--- app/api/search_routes.py
+++ app/api/search_routes.py
@@ -6,8 +6,10 @@ search_route = Blueprint('search', __name__)
 @search_route.route('/<keyword>')
 def do_search(keyword):
     print('is this working?')
-    title_keyword = Cheatsheet.query.filter(Cheatsheet.title.ilike(f'%{keyword}%')).all()
+    # title_keyword = Cheatsheet.query.filter(Cheatsheet.title.ilike(f'%{keyword}%')).all()
+    # title_list = [cheatsheet.to_dict() for cheatsheet in title_keyword]
 
-    title_list = [cheatsheet.to_dict() for cheatsheet in title_keyword]
+    cheatsheets = Cheatsheet.query.filter(Cheatsheet.title.ilike(f'%{keyword}%')).all()
+    cheatsheet_list = [cheatsheet.to_dict() for cheatsheet in cheatsheets]
 
-    return {'title_keyword': title_list}
+    return {'cheatsheet_by_title': cheatsheet_list}
