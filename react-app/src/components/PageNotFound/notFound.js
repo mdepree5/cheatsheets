b3a82531ec5116c95639bd22476a4a87b2bb4f27
daniel <danielthai14@gmail.com>
2022-03-11 13:51:41 -0700

    auto redirect in 404 page

--- react-app/src/components/PageNotFound/notFound.js
+++ react-app/src/components/PageNotFound/notFound.js
@@ -1,12 +1,23 @@
+import React, {useEffect} from 'react'
+import { useHistory } from 'react-router-dom';
 import './notFound.css';
 import { NavLink } from 'react-router-dom';
 
 const PageNotFound = () => {
+    const history = useHistory();
+
+    useEffect(() => {
+    setTimeout(() => {
+        history.push('/')
+        }, 4000)
+    }, [history])
+
+
     return (
         <div className='error-container'>
             <h2 className='not-found'>404: We're sorry, code breaks sometimes</h2>
             <div className='alt-container'>
-                <p className='alt-link-p'>Why don't you try this instead.</p>
+                <p className='alt-link-p'>Go home or we will redirect you shortly.</p>
                 <div className='alt-button-div'>
                     <NavLink className={'alt-button'} to='/'>Home</NavLink>
                 </div>
