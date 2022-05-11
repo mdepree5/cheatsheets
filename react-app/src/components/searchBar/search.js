146e9e25dfd47630626e86036a19b595dc365fe9
Mitch DePree <mdepree315@gmail.com>
2022-05-11 16:12:22 -0400

    refactor nav

--- react-app/src/components/searchBar/search.js
+++ react-app/src/components/searchBar/search.js
@@ -7,32 +7,23 @@ import './search.css'
 const SearchBar = () => {
     const dispatch = useDispatch();
     const history = useHistory();
-
     const [ keyword, setKeyword ] = useState('')
 
     const handleSubmit = async (e) => {
-        e.preventDefault();
-        await dispatch(getSearchResults(keyword));
-        history.push(`/search/${keyword}`);
-
-        setKeyword('')
+      e.preventDefault();
+      await dispatch(getSearchResults(keyword));
+      history.push(`/search/${keyword}`);
+      setKeyword('')
     }
 
 
-    return (
-        <>
-            <form className='search_form'>
-                <input
-                    type="text"
-                    className='search_input'
-                    placeholder='what do you wanna know...'
-                    value={keyword}
-                    onChange={e => setKeyword(e.target.value)}
-                />
-                <button className='search-btn' onClick={handleSubmit}>search</button>
-            </form>
-        </>
-    )
+  return (
+    <form className='search_form'>
+      <input type="text" className='search_input' placeholder='Teach me how to...'
+        value={keyword} onChange={e => setKeyword(e.target.value)} />
+      <button className='search-btn' onClick={handleSubmit}>Search</button>
+    </form>
+  )
 }
 
 export default SearchBar;
